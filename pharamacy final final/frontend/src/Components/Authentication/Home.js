5f3c80111e568caa86602b70844fe893a32c23fb
wICKRAMASINGHAS <150247013+wICKRAMASINGHAS@users.noreply.github.com>
2024-05-12 15:59:44 +0530

    front end
    front end edits

--- pharamacy final final/frontend/src/Components/Authentication/Home.js
+++ pharamacy final final/frontend/src/Components/Authentication/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import heroImage from './heroImage.jpg';
-import half1 from './half1.png';
+import explore from './explore.png';
 
 
 
@@ -17,37 +17,27 @@ const Home = () => {
         textAlign: 'center', 
         paddingTop: '200px', 
         color: 'white' // Set text color to white
+      
       }}
     >
       <h1>Welcome to HealthFirst</h1>
       <p>Find all your medication needs in one place</p>
     </section>
-    <section style={{ height: '500px', backgroundColor: '#ffffff', textAlign: 'center', paddingTop: '50px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-  {/* First row */}
-  <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '50px' }}>
-    {/* Left side with image */}
-    <div style={{ flex: 1 }}>
-    <img src={half1} alt="Description of the image" style={{ maxWidth: '1200px', height: '500px', width:'100%' }} />
 
-    </div>
-    {/* Right side with text */}
-    <div style={{ flex: 1, paddingLeft: '20px', textAlign: 'left' }}>
-      <h2>Explore</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et bibendum velit.</p>
-    </div>
-  </div>
+
+ <section style={{ height: '800px', backgroundColor: '#ffe9e1', textAlign: 'center', paddingTop: '0px' }}>
+  <img src={explore} alt="Explore Image" style={{ width: '100%' }} />
 </section>
 
 
-
       {/* About Us section */}
-      <section style={{ height: '800pxS', backgroundColor: '#ffe9e1', textAlign: 'center', paddingTop: '200px' }}>
+      <section style={{ height: '500px', backgroundColor: '#ffe9e1', textAlign: 'center', paddingTop: '100px' }}>
         <h2>About Us</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et bibendum velit.</p>
       </section>
 
       {/* Contact Us section */}
-      <section style={{ height: '800px', backgroundColor: '#f0f0f0', textAlign: 'center', paddingTop: '200px' }}>
+      <section style={{ height: '500px', backgroundColor: '#f0f0f0', textAlign: 'center', paddingTop: '200px' }}>
         <h2>Contact Us</h2>
         <p>Email: info@example.com</p>
         <p>Phone: 123-456-7890</p>
