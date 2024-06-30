0d2ca022635dcf0e58dc43559980dfbe141c972b
Tashi-01 <taniya.pathirana01@gmail.com>
2024-06-30 22:20:01 +0530

    basic_2

--- pharamacy final final/frontend/src/App.js
+++ pharamacy final final/frontend/src/App.js
@@ -41,7 +41,9 @@ import CheckoutPage from "./pages/Product/CheckoutPage";
 import Navbar from './Components/Product/Navbar';
 import Navbar2 from './Components/Inquiry/Navbar';
 import DeliveryPage from './pages/Product/DeliveryPage';
+
 // import UploadItems from './pages/Product/UploadItem';
+
 
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
